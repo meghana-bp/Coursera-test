7089de6b5989c21dde75aa6be248cb38dd290dc1
B P <meghana.b-p@capgemini.com>
2021-07-14 17:17:34 +0530

    modification

---
+++ Module-4-Assignment/js/script.js
@@ -0,0 +1,10 @@
+var names = ["Meghana","John","jay","Maxie","Sonia","Jason", "Loura","Arha","jessica","Jamie"]
+
+for(var i=0;i<names.length;i++){
+	 if(names[i].charAt(0)==='J'|| names[i].charAt(0)=="j"){
+	 	console.log("Goodbye "+names[i]);
+	 }
+	 else{
+	 	console.log("Hello "+names[i]);
+	 }
+	}
